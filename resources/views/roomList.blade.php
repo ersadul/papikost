88cf01feaf64f51b676b4ac54fbaad50a264726a
Harun Ulum Fajar <hufmalang@gmail.com>
2019-05-19 16:13:47 +0700

    Set Room Detail udah Bisa

--- resources/views/roomList.blade.php
+++ resources/views/roomList.blade.php
@@ -42,8 +42,8 @@
                 </div>
                 <div class="col-xl-2 col-lg-3">
                     <div class="for-booking">
-                        <div class="h5 per-night text-primary">Rp. {{$k->harga}}<small> / Night</small></div>
-                        <form action="{{ route('room.detail', ['id' => $k->id_kamar]) }}" method="post">
+                        <div class="h5 per-night text-primary">Rp. {{$k->harga}}<small> / Night {{$k->id}}</small></div>
+                        <form action="{{ route('room.detail', ['id' => $k->id]) }}" method="post">
                             @csrf
                             <input type="hidden" name="checkIn" value="{{$checkIn}}">
                             <input type="hidden" name="lamaMenginap" value="{{$lamaMenginap}}">
