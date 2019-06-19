6ddfd796935d38f752b596f06574aa61add2ec3a
Harun0813 <hufmalang@gmail.com>
2019-06-19 14:45:21 +0700

    Fix bug Promo

--- resources/views/promo.blade.php
+++ resources/views/promo.blade.php
@@ -46,26 +46,6 @@
             </div>
             @endforeach
         </div>
-        <div class="row">
-            <div class="col-lg-12">
-                <nav class="x-center" aria-label="Page navigation">
-                    <ul class="pagination">
-                        <li class="page-item disabled">
-                            <a class="page-link" href="#" tabindex="-1"><i class="fas fa-chevron-left"></i></a>
-                        </li>
-                        <li class="page-item active">
-                            <a class="page-link" href="#">1 <span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="page-item"><a class="page-link" href="#">2</a></li>
-                        <li class="page-item">...</li>
-                        <li class="page-item"><a class="page-link" href="#">10</a></li>
-                        <li class="page-item">
-                            <a class="page-link" href="#"><i class="fas fa-chevron-right"></i></a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </div>
     </div>
 </section>
 @endsection
